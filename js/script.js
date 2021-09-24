a3013dd20f962b15df4fb1155684222fd0873e4f
Marco Luciano <maurelio.sluciano@gmail.com>
2021-09-23 23:25:58 -0300

    Segunta alteração JS do menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-function abrirMenu() {
+/*function abrirMenu(){
+    document.querySelector('.menu-toggle').style.display = 'block'
     document.querySelector('.menu-toggle').classList.add('.menu-opened')
-    if(document.querySelector('.menu-toggle').classList.contains('.menu-opened') == true) {
+
+    let toggle = document.querySelector('.menu-opener').addEventListener('click', function(){
+        document.querySelector('.menu-toggle').classList.remove('.menu-opened')
+        document.querySelector('.menu-toggle').style.display = 'none'
+    })
+}*/
+
+function abrirMenu(){
+    let openMenu = document.querySelector('.menu-toggle').style.display = 'block'
+    if(openMenu.style.display == 'block') {
         document.querySelector('.menu-toggle').style.display = 'block'
+    } else {
+        document.querySelector('.menu-toggle').addEventListener('click', function(){
+            document.querySelector('.menu-toggle').style.display = 'none'
+        })
     }
 }

cffd86a44c91e533dea4d8919b2387882602afac
Marco Luciano <maurelio.sluciano@gmail.com>
2021-10-20 23:45:19 -0300

    Update index, css and Js files

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@ $menu.addEventListener('click', () => {
         $menuOpen.classList.remove('menu-show')
     }
 })
+
+
+function abrirModal() {
+    let abm = document.querySelector('.bg-modal')
+    abm.style.top = '0'
+}
+
+/*function fecharModal() {
+    let fm = document.querySelector('.bg-modal')
+    fm.style.top = '-100%'
+}
+
+
+/*let abrirModal = document.querySelector('.btn-video')
+abrirModal.addEventListener('click', ()=>{
+    abrirModal.style.top = '0'
+})
+
+let fecharModal = document.querySelector('.close')
+fecharModal.addEventListener('click', ()=> {
+    fecharModal.style.top = '-100%'
+})*/
